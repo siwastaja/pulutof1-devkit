71da4a724047318d7f47e0545153ae2787576cd9
Antti Alhonen <antti.alhonen@iki.fi>
2018-05-16 12:21:10 +0300

    devkit sensor locations

--- pulutof.c
+++ pulutof.c
@@ -258,10 +258,10 @@ typedef struct
 
 static const sensor_mount_t sensor_mounts[NUM_PULUTOFS] =
 {          //      mountmode    x     y       hor ang           ver ang      height    
- /*0: Left rear      */ { 2,  -276,  -233, DEGTORAD(      90), DEGTORAD(  0), 227 },
- /*1: Right rear     */ { 1,  -276,   233, DEGTORAD(     270), DEGTORAD(  0), 227 },
- /*2: Right front    */ { 2,   154,   164, DEGTORAD(       0), DEGTORAD(  0), 228 },
- /*3: Left front     */ { 1,   154,  -164, DEGTORAD(       0), DEGTORAD(  0), 228 }
+ /*0:                */ { 4,    70,     0, DEGTORAD(       0), DEGTORAD(  0),   0 },
+ /*1:                */ { 4,     0,   -70, DEGTORAD(      90), DEGTORAD(  0),   0 },
+ /*2:                */ { 4,   -70,     0, DEGTORAD(     180), DEGTORAD(  0),   0 },
+ /*3:                */ { 4,     0,    70, DEGTORAD(     270), DEGTORAD(  0),   0 }
 };
 
 static void distances_to_objmap(pulutof_frame_t *in)
